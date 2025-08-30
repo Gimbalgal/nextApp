892d8393bc2f147b8b1c75c6c43c520f1afad221
Gimbalgal <victoriaishabo55@gmail.com>
2025-08-30 12:06:01 +0100

    another commit

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,6 @@
 export const runtime = "nodejs";
 
-import bcrypt from "bcryptjs";
+import  bcrypt from "bcryptjs";
 import postgres, { Sql } from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
